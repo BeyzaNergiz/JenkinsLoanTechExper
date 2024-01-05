c207564f801d9f4346df176f23a1da563132f318
Beyza Nergiz <byz.nergiz34@gmail.com>
2024-01-05 16:21:46 +0300

    nergiz

--- src/test/java/runners/API_Runner.java
+++ src/test/java/runners/API_Runner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "pretty",
                 "html:target/default-cucumber-reports.html",
-                "json:target/json-reports/cucumber.json",
+                "json:target/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt"
         },
